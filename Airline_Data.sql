5fc2bcdf20a5bb4685c0c44c6c69bfffce1252dc
luckykismat86 <luckykismat86@gmail.com>
2021-08-11 15:03:37 -0400

    heres something

---
+++ Airline_Data.sql
@@ -0,0 +1,18 @@
+[
+  {
+    name: "Spirit",
+    destinations_flown_to: ["New Zealand", "Scotland"]
+  },
+  {
+    name: "Lufthansa",
+    destinations_flown_to: ["Tristan da Cunha", "Scotland", "Thailand"]
+  },
+  {
+    name: "Delta",
+    destinations_flown_to: ["Thailand", "Minnesota", "England", "Scotland"]
+  },
+  {
+    name: "Southwest"
+    destinations_flown_to: ["New Zealeand", "Tristan de Cunha", "Minnesota"]
+  }
+]
